2097cbc04a1b3ba513f55e849be10ba9550dbe23
Boyan Kushlev <takarabukara@gmail.com>
2017-05-15 21:56:40 +0300

    fixing format of quesitons and adding new functions

--- xhtml/format.php
+++ xhtml/format.php
@@ -37,11 +37,11 @@ defined('MOODLE_INTERNAL') || die();
 class qformat_xhtml extends qformat_default {
 
     private $pdf = null;
-	
-	public function __construct() {
+
+    public function __construct() {
         $this->pdf = $this->get_pdf_generator_instance();
     }
-	
+
     public function provide_export() {
         return true;
     }
@@ -59,49 +59,74 @@ class qformat_xhtml extends qformat_default {
         if ($question->qtype=='category') {
             return '';
         }
-        
-        
+
         // Initial string.
         $expout = "";
         $id = $question->id;
 
-        $expout .= $question->name . " ";
-		$expout .= strip_tags($question->questiontext);
-        $expout .= "\n";
-		
+        // format the question name and text
+        switch($question->qtype) {
+            case 'truefalse':
+            case 'shortanswer':
+                $this->write_question_name($question);
+                $expout .= $this->tab() . strip_tags($question->questiontext); // the text of the question
+                $expout .= "\n\n";
+                break;
+            case 'multichoice':
+                $this->write_question_name($question);
+                $expout .= $this->tab() . strip_tags($question->questiontext); // the text of the question
+                break;
+            case 'numerical':
+                break;
+            case 'match':
+                break;
+            case 'description':
+                break;
+            case 'multianswer':
+                break;
+            case 'calculated':
+                break;
+            case 'calculatedmulti':
+                break;
+            case 'calculatedsimple':
+                break;
+            case 'essay':
+                break;
+            case 'gapselect':
+                break;
+            // for all unsupported question types add an HTML comment (just in case) and return nothing
+            default:
+                $expout .= "<!-- export of {$question->qtype} type is not supported  -->\n";
+                $this->pdf->WriteHTML($expout, true, false, true, false, '');
+                return '';
+
+        }
+
+        $this->pdf->Write(5, $expout, '', 0, 'L', true, 0, false, false, 0);
+        $expout = "";
+
         // Selection depends on question type.
         switch($question->qtype) {
             case 'truefalse':
-            	$sttrue = get_string('true', 'qtype_truefalse');
-                $stfalse = get_string('false', 'qtype_truefalse');
-                
-                $expout .= $this->tab() . $sttrue . "\n";
-                $expout .= $this->tab() . $stfalse . "\n";
-                $expout .= "\n";
+                $expout .= $this->tab() . 'True' . $this->tab();
+                $expout .= $this->tab() . 'False';
+                $expout .= $this->gap_between_questions();
                 $this->pdf->Write(5, $expout, '', 0, 'L', true, 0, false, false, 0);
-               
+
                 break;
             case 'multichoice':
-                $expout .= "<ul class=\"multichoice\">\n";
+                $expout .= "<ol style=\"list-style-type:upper-alpha\" class=\"multichoice\">\n";
                 foreach ($question->options->answers as $answer) {
                     $answertext = $this->repchar( $answer->answer );
-                    if ($question->options->single) {
-                        $expout .= "  <li><input name=\"quest_{$id}\" type=\"radio\" value=\""
-                                . s($answertext) . "\" />{$answertext}</li>\n";
-                    } else {
-                        $expout .= "  <li><input name=\"quest_{$id}\" type=\"checkbox\" value=\""
-                                . s($answertext) . "\" />{$answertext}</li>\n";
-                    }
+                    $expout .= "  <li>{$answertext}</li>";
                 }
-                $expout .= "</ul>\n";
+                $expout .= "</ol>";
+                $this->pdf->WriteHTML($expout, false, false, true, false, '');
                 break;
             case 'shortanswer':
-                $expout .= html_writer::start_tag('ul', array('class' => 'shortanswer'));
-                $expout .= html_writer::start_tag('li');
-                $expout .= html_writer::label(get_string('answer'), 'quest_'.$id, false, array('class' => 'accesshide'));
-                $expout .= html_writer::empty_tag('input', array('id' => "quest_{$id}", 'name' => "quest_{$id}", 'type' => 'text'));
-                $expout .= html_writer::end_tag('li');
-                $expout .= html_writer::end_tag('ul');
+                $expout .= $this->tab() . '_______________________________________________________________________________';
+                $expout .= $this->gap_between_questions();
+                $this->pdf->Write(5, $expout, '', 0, 'L', true, 0, false, false, 0);
                 break;
             case 'numerical':
                 $expout .= html_writer::start_tag('ul', array('class' => 'numerical'));
@@ -113,7 +138,7 @@ class qformat_xhtml extends qformat_default {
                 break;
             case 'match':
                 $expout .= html_writer::start_tag('ul', array('class' => 'match'));
-				
+
                 // Build answer list.
                 $answerlist = array();
                 foreach ($question->options->subquestions as $subquestion) {
@@ -147,35 +172,48 @@ class qformat_xhtml extends qformat_default {
             case 'description':
                 break;
             case 'multianswer':
+                break;
+            case 'calculated':
+                break;
+            case 'calculatedmulti':
+                break;
+            case 'calculatedsimple':
+                break;
+            case 'essay':
+                break;
+            case 'gapselect':
+                break;
             default:
-        		
                 $expout .= "<!-- export of {$question->qtype} type is not supported  -->\n";
         }
-		$expout .= "</tr>";
+		// $expout .= "</tr>";
         return $expout;
     }
 
     protected function presave_process($body) {
         // Convert to pdf
-		$pdf_file = $this->pdf->Output('questions.pdf', 's');
+	      $pdf_file = $this->pdf->Output('questions.pdf', 's');
         return $pdf_file;
     }
-    
+
     private function get_pdf_generator_instance() {
         global $CFG;
-
         require_once $CFG->libdir . "/tcpdf/tcpdf.php";
-        $fontpath = $CFG->dirroot . "/question/format/xhtml/fonts/OpenSans-Regular.ttf";
-    
+
         $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+
+        // set up the font
+        $fontpath = $CFG->dirroot . "/question/format/xhtml/fonts/OpenSans-Regular.ttf";
+        global $fontname;
+        $fontname = TCPDF_FONTS::addTTFfont($fontpath, 'TrueTypeUnicode', '', 32);
+        $pdf->SetFont($fontname, '', 11, '', false);
+
         $pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
         $pdf->SetCellPadding(0);
         // set auto page breaks
         $pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
         // set image scale factor
         $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
-        $fontname = TCPDF_FONTS::addTTFfont($fontpath, 'TrueTypeUnicode', '', 32);
-        $pdf->SetFont($fontname, '', 11, '', false);
         // add a page
         $pdf->AddPage();
 
@@ -184,6 +222,18 @@ class qformat_xhtml extends qformat_default {
 
     private function tab() {
         return "    ";
+    }
+
+    private function gap_between_questions() {
+        return "\n\n\n\n";
+    }
+
+    private function write_question_name($question) {
+      $text = "";
+      $text .= $question->name . ":\n";  // the "name" of the question
+      $this->pdf->SetFont($fontname, 'B', 13);
+      $this->pdf->Write(5, $text, '', 0, 'L', true, 0, false, false, 0);
+      $this->pdf->SetFont($fontname, '', 11);
     }
 
     public function export_file_extension() {

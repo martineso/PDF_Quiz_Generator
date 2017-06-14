ac718eeeb3ee3f283d2e5e423fd1d8df95b13e17
Boyan Kushlev <takarabukara@gmail.com>
2017-06-14 19:57:35 +0300

    adding student name and date

--- xhtml/lang/en/qformat_xhtml.php
+++ xhtml/lang/en/qformat_xhtml.php
@@ -22,6 +22,8 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+$string['student_name'] = 'Name: ';
+$string['date'] = 'Date: ' . date("Y-m-d");
 $string['pluginname'] = 'XHTML format';
 $string['pluginname_help'] = 'XHTML format enables all questions in the category to be exported to a single page of strict XHTML for possible use in another application.';
 $string['pluginname_link'] = 'qformat/xhtml';
